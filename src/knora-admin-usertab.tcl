e22fe561f9b47e81b38a1e60db739cafb6624bf7
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2018-04-11 01:58:59 +0200

    More code flxibilisation

--- src/knora-admin-usertab.tcl
+++ src/knora-admin-usertab.tcl
@@ -1,111 +1,179 @@
 
-global field_status
+global languages
+
+global fielddefs
+set fielddefs [dict create \
+    familyName [list entryfield "Lastname"] \
+    givenName [list entryfield "Firstname"] \
+    email [list entryfield "Email"] \
+    password [list passwdfield "Password"] \
+    lang [list pulldown "Language" $languages]
+]
+
+
+#
+# the proc is called if an entry field is being changed
+#
 proc field_changed {varname args} {
-  global user_right
-  set field_status($varname) 1
-  $user_right.save configure -state normal
+    global user_right
+    global orig_value
+    global field_status
+
+    set fieldname [lindex $args 0]
+    set field_status($fieldname) true
+    $user_right.save configure -state normal
+    $user_right.${fieldname}S configure -state normal
+}
+
+proc undo { fieldname } {
+    global fielddefs
+    global user_right
+    global orig_value
+    global field_status
+    global value
+
+    set fieldinfo [dict get $fielddefs $fieldname]
+    switch [lindex $fieldinfo 0] {
+        entryfield {}
+        passwdfield {}
+        pulldown {
+            $user_right.${fieldname}E configure -text $orig_value($fieldname)
+        }
+    }
+    set value($fieldname) $orig_value($fieldname)
+    $user_right.${fieldname}S configure -state disabled
+    set field_status($fieldname) 0
+    
+    set save false
+    foreach { key val } [array get field_status ] {
+        if { $val } {
+            set save true
+        }
+    }
+    if { !$save } {
+        $user_right.save configure -state disable
+    }
 }
 
 
 proc users_tab { users_w } {
-  global knora_server
-  global userlist
-  global userarr
-  global user_right
-  global languages
-
-  global familyNameV
-  global givenNameV
-  global emailV
-  global passwordV
-
-  set user_paned [ttk::panedwindow $users_w.paned -orient horizontal]
-  set user_left [ttk::labelframe $user_paned.left  -text "Userlist"]
-  set user_right [ttk::labelframe $user_paned.right -text "Details"]
-  $user_paned add $user_left
-  $user_paned add $user_right
-
-  set users [knoraApi::get_users]
-  foreach user $users {
-    set userarr([dict get $user id]) $user
-  }
-
-  set userlist [ttk::treeview $user_left.userlist \
+    global knora_server
+    global userlist
+    global userarr
+    global user_right
+    
+    global fielddefs
+    global field_status
+    global orig_value
+    global value
+        
+    set user_paned [ttk::panedwindow $users_w.paned -orient horizontal]
+    set user_left [ttk::labelframe $user_paned.left  -text "Userlist"]
+    set user_right [ttk::labelframe $user_paned.right -text "Details"]
+    $user_paned add $user_left
+    $user_paned add $user_right
+    
+    set users [knoraApi::get_users]
+    foreach user $users {
+        set userarr([dict get $user id]) $user
+    }
+    
+    set userlist [ttk::treeview $user_left.userlist \
     -show headings \
     -selectmode browse \
     -columns [list last first id] \
     -displaycolumns [list last first id]]
-  $userlist heading last -text Lastname
-  $userlist heading first -text Firstname
-  $userlist heading id -text Id
-  bind $userlist <<TreeviewSelect>> {
-    global userlist
-    global userarr
-    global user_right
-    set uid [$userlist focus]
-    set familyNameV [dict get $userarr($uid) familyName]
-    set field_status(familyNameV) 0
-    set givenNameV [dict get $userarr($uid) givenName]
-    set field_status(givenNameV) 0
-    set emailV [dict get $userarr($uid) email]
-    set field_status(emailV) 0
-    set passwordV {} ;# empty password field
-    set field_status(passwordV) 0
-    $user_right.langE configure -text [dict get $userarr($uid) lang]
-    $user_right.save configure -state disabled
+    $userlist heading last -text Lastname
+    $userlist heading first -text Firstname
+    $userlist heading id -text Id
+    bind $userlist <<TreeviewSelect>> {
+        global userlist
+        global userarr
+        global user_right
+        set uid [$userlist focus]
+        
+        dict for {fieldname fieldinfo} $fielddefs {
+            set value($fieldname) [dict get $userarr($uid) $fieldname]
+            set orig_value($fieldname) $value($fieldname)
+            set field_status($fieldname) 0
+            switch [lindex $fieldinfo 0] {
+                entryfield {}
+                passwdfield {}
+                pulldown {
+                    set value($fieldname) [dict get $userarr($uid) $fieldname]
+                    set orig_value($fieldname) $value($fieldname)
+                    set field_status($fieldname) 0
+                    $user_right.${fieldname}E configure -text [dict get $userarr($uid) lang]
+                }
+            }
+            $user_right.${fieldname}S configure -state disabled
+        }
+        $user_right.save configure -state disabled
+    }
+    
+    foreach user $users {
+        set iri [dict get $user id]
+        set id [lindex [helper::parse_iri $iri] end]
+        $userlist insert {} end -id $iri \
+        -values [list [dict get $user familyName] [dict get $user givenName] $iri]
+    }
+    pack $userlist -fill y -expand 1
+    
+    dict for {fieldname fieldinfo} $fielddefs {
+        switch [lindex $fieldinfo 0] {
+            entryfield {
+                ttk::label $user_right.${fieldname}L -text [lindex $fieldinfo 1]
+                ttk::entry $user_right.${fieldname}E -textvariable value($fieldname)
+                ttk::button $user_right.${fieldname}S -text {undo} -command "undo $fieldname"
+                trace variable value($fieldname) w field_changed
+            }
+            passwdfield {
+                ttk::label $user_right.${fieldname}L -text [lindex $fieldinfo 1]
+                ttk::entry $user_right.${fieldname}E -textvariable value($fieldname) -show {*}
+                ttk::button $user_right.${fieldname}S -text {undo} -command "undo $fieldname"
+                trace variable value($fieldname) w field_changed
+            }
+            pulldown {
+                ttk::label $user_right.${fieldname}L -text [lindex $fieldinfo 1]
+                ttk::menubutton $user_right.${fieldname}E -menu $user_right.${fieldname}E.m -text "-"
+                menu $user_right.${fieldname}E.m
+    
+                foreach lang [lindex $fieldinfo 2] {
+                    $user_right.${fieldname}E.m add command -label $lang -command "
+                    set value($fieldname) $lang
+                    $user_right.${fieldname}E configure -text $lang
+                    "
+                }
+                ttk::button $user_right.${fieldname}S -text {undo} -command "undo $fieldname"
+                trace variable value($fieldname) w field_changed
+            }
+        }
+    }
 
-  }
-
-  foreach user $users {
-    set iri [dict get $user id]
-    set id [lindex [helper::parse_iri $iri] end]
-    $userlist insert {} end -id $iri \
-      -values [list [dict get $user familyName] [dict get $user givenName] $iri]
-  }
-  pack $userlist -fill y -expand 1
-
-  ttk::label $user_right.familyNameL -text "Lastname:"
-  ttk::entry $user_right.familyNameE -textvariable familyNameV
-  trace variable familyNameV w field_changed
-  ttk::label $user_right.givenNameL -text "Firstname:"
-  ttk::entry $user_right.givenNameE -textvariable givenNameV
-  trace variable givenNameV w field_changed
-  ttk::label $user_right.emailL -text "Email:"
-  ttk::entry $user_right.emailE -textvariable emailV
-  trace variable emailV w field_changed
-  ttk::label $user_right.passwordL -text "Password:"
-  ttk::entry $user_right.passwordE -show 0 -textvariable passwordV
-  trace variable passwordV w field_changed
-  ttk::label $user_right.langL -text "Language:"
-  ttk::menubutton $user_right.langE -menu $user_right.langE.m -text "-"
-  menu $user_right.langE.m
-
-  foreach lang $languages {
-    $user_right.langE.m add command -label $lang -command {}
-  }
-
-  ttk::button $user_right.save \
+    
+    ttk::button $user_right.save \
     -text "Save" \
     -state disabled \
     -command {}
-
+    
     ttk::button $user_right.new \
-      -text "new" \
-      -command {}
-
-
-  grid $user_right.familyNameL -column 0 -row 0 -sticky ne
-  grid $user_right.familyNameE -column 1 -row 0 -sticky nw
-  grid $user_right.givenNameL -column 0 -row 1 -sticky ne
-  grid $user_right.givenNameE -column 1 -row 1 -sticky nw
-  grid $user_right.emailL -column 0 -row 2 -sticky ne
-  grid $user_right.emailE -column 1 -row 2 -sticky nw
-  grid $user_right.passwordL -column 0 -row 3 -sticky ne
-  grid $user_right.passwordE -column 1 -row 3 -sticky nw
-  grid $user_right.langL -column 0 -row 4 -sticky ne
-  grid $user_right.langE -column 1 -row 4 -sticky nw
-  grid $user_right.save -column 0 -row 5 -sticky ne
-  grid $user_right.new -column 1 -row 5 -sticky ne
-
-  pack $user_paned -fill both -expand 1
+    -text "new" \
+    -command {}
+    
+    set i 0
+    dict for {fieldname fieldinfo} $fielddefs {
+        grid $user_right.${fieldname}L -column 0 -row $i -sticky ne
+        grid $user_right.${fieldname}E -column 1 -row $i -sticky nw
+        grid $user_right.${fieldname}S -column 2 -row $i -sticky nw
+        incr i
+    }
+    grid $user_right.langL -column 0 -row 4 -sticky ne
+    grid $user_right.langE -column 1 -row 4 -sticky nw
+    grid $user_right.save -column 0 -row 5 -sticky ne
+    grid $user_right.new -column 1 -row 5 -sticky ne
+    
+    pack $user_paned -fill both -expand 1
+    
+    set json [::json::write object gaga GAGA gugus 5 ARR [::json::write arra AA BB CC DD]]
+    puts $json
 }
--- src/knora-admin.tcl
+++ src/knora-admin.tcl
@@ -6,6 +6,7 @@ set execdir [file dirname $argv0]
 
 package require rest
 package require json
+package require json::write
 package require Img
 package require xml
 package require tooltip
@@ -16,6 +17,9 @@ set knora_server "http://0.0.0.0:3333"
 
 global aeskey
 set aeskey "ABCDEFGHIJKLMOPQ"
+
+global languages ;# list of supported languages
+set languages [list de en fr it]
 
 #
 # here we include some library-type sources
@@ -33,8 +37,6 @@ set username "-"
 global aespasswd
 set aespasswd ""
 
-global languages ;# list of supported languages
-set languages [list de en fr it]
 
 #*****************************************************************************
 # proc startup_box {}
@@ -44,186 +46,188 @@ set languages [list de en fr it]
 # Creates the startup dialog box and puts it onto the screen
 #*****************************************************************************
 proc startup_box {} {
-  global private startup_box_w
-  global private tmppasswd
-  global private tmpusername
-  global private login_error_l
-  global http_code
-
-  set tmpusername ""
-  set tmppasswd ""
-  set startup_box_w [toplevel .startup -class Dialog]
-  wm resizable $startup_box_w false false
-  wm sizefrom $startup_box_w program
-  wm group $startup_box_w .
-  wm transient $startup_box_w [winfo toplevel [winfo parent $startup_box_w]]
-  wm title $startup_box_w "knora Administration Console 0.1.0 BETA"
-
-  set geostr [wm geometry .]
-  regexp {([0-9]*)x([0-9]*)(\+)([0-9]*)(\+)([0-9]*)} $geostr all width height p1 pos_x p2 pos_y
-  set pos_x [expr $pos_x + $width / 2 - 200]
-  set pos_y [expr $pos_y + $height / 2 - 100]
-  wm geometry $startup_box_w "+$pos_x+$pos_y"
-
-  set startup_label_w [label $startup_box_w.label \
-    -text {  Knora Admin Console }]
-
-  set startup_date_w [label $startup_box_w.date \
-    -text "Version 0.1.0 BETA"]
-
-  set startup_info1_w [label $startup_box_w.info1 \
-    -wraplength 4i \
-    -justify left \
-    -text "Data and Service Center for the Humanities (DaSCH)"]
-
-  set startup_info2_w [label $startup_box_w.info2 \
-    -text {lukas.rosenthaler@unibas.ch}]
-
-  set login_frame_w [frame $startup_box_w.frame \
-    -relief sunken \
-    -borderwidth 2]
-  set login_user_l [label $login_frame_w.user_l -text "Username:"]
-  set login_user_e [ttk::entry $login_frame_w.user_e -textvariable tmpusername]
-  set login_passwd_l [label $login_frame_w.passwd_l -text "Password:"]
-  set login_passwd_e [ttk::entry $login_frame_w.passwd_e  -show {*} -textvariable tmppasswd]
-  set login_error_l [label $login_frame_w.error_l -foreground "red"]
-  grid $login_user_l -column 0 -row 0
-  grid $login_user_e -column 1 -row 0
-  grid $login_passwd_l -column 0 -row 1
-  grid $login_passwd_e -column 1 -row 1
-  grid $login_error_l -column 0 -columnspan 2 -row 3
-
-#  pack $login_label_w -fill x -expand 1 -side top
-#  pack $login_frame_w -fill both -expand 1
-
-  global private login_counter
-  set login_counter 0
-  set startup_dismiss_w [button $startup_box_w.dismiss \
-    -text "OK" \
-    -default active \
-    -command {
-      set username $tmpusername
-      set aespasswd [aes::aes -dir encrypt -key $aeskey $tmppasswd]
-      set res [knoraApi::authenticate]
-      if { [dict exists $res errormsg] } {
-        if { $login_counter < 3 } {
-          $login_error_l configure -text [dict get $res errormsg]
-        } else {
-          exit
-        }
-      } else {
-        set private(dismiss) 1
-      }
-      incr login_counter
-    }]
-  bind $startup_dismiss_w <Return> "
-    $startup_dismiss_w configure -state active -relief sunken
-    update idletasks
-    after 100
-    $startup_dismiss_w invoke
-  "
-
-  pack $startup_label_w \
-    $startup_date_w \
+    global private startup_box_w
+    global private tmppasswd
+    global private tmpusername
+    global private login_error_l
+    global http_code
+    
+    set tmpusername ""
+    set tmppasswd ""
+    set startup_box_w [toplevel .startup -class Dialog]
+    wm resizable $startup_box_w false false
+    wm sizefrom $startup_box_w program
+    wm group $startup_box_w .
+    wm transient $startup_box_w [winfo toplevel [winfo parent $startup_box_w]]
+    wm title $startup_box_w "knora Administration Console 0.1.0 BETA"
+    
+    set geostr [wm geometry .]
+    regexp {([0-9]*)x([0-9]*)(\+)([0-9]*)(\+)([0-9]*)} $geostr all width height p1 pos_x p2 pos_y
+    set pos_x [expr $pos_x + $width / 2 - 200]
+    set pos_y [expr $pos_y + $height / 2 - 100]
+    wm geometry $startup_box_w "+$pos_x+$pos_y"
+    
+    set startup_label_w [label $startup_box_w.label \
+			     -text {  Knora Admin Console }]
+    
+    set startup_date_w [label $startup_box_w.date \
+			    -text "Version 0.1.0 BETA"]
+    
+    set startup_info1_w [label $startup_box_w.info1 \
+			     -wraplength 4i \
+			     -justify left \
+			     -text "Data and Service Center for the Humanities (DaSCH)"]
+    
+    set startup_info2_w [label $startup_box_w.info2 \
+			     -text {lukas.rosenthaler@unibas.ch}]
+    
+    set login_frame_w [frame $startup_box_w.frame \
+			   -relief sunken \
+			   -borderwidth 2]
+    set tmpusername "root@example.com"
+    set tmppasswd "test"
+    set login_user_l [label $login_frame_w.user_l -text "Username:"]
+    set login_user_e [ttk::entry $login_frame_w.user_e -textvariable tmpusername]
+    set login_passwd_l [label $login_frame_w.passwd_l -text "Password:"]
+    set login_passwd_e [ttk::entry $login_frame_w.passwd_e  -show {*} -textvariable tmppasswd]
+    set login_error_l [label $login_frame_w.error_l -foreground "red"]
+    grid $login_user_l -column 0 -row 0
+    grid $login_user_e -column 1 -row 0
+    grid $login_passwd_l -column 0 -row 1
+    grid $login_passwd_e -column 1 -row 1
+    grid $login_error_l -column 0 -columnspan 2 -row 3
+    
+    #  pack $login_label_w -fill x -expand 1 -side top
+    #  pack $login_frame_w -fill both -expand 1
+    
+    global private login_counter
+    set login_counter 0
+    set startup_dismiss_w [button $startup_box_w.dismiss \
+			       -text "OK" \
+			       -default active \
+			       -command {
+				   set username $tmpusername
+				   set aespasswd [aes::aes -dir encrypt -key $aeskey $tmppasswd]
+				   set res [knoraApi::authenticate]
+				   if { [dict exists $res errormsg] } {
+				       if { $login_counter < 3 } {
+					   $login_error_l configure -text [dict get $res errormsg]
+				       } else {
+					   exit
+				       }
+				   } else {
+				       set private(dismiss) 1
+				   }
+				   incr login_counter
+			       }]
+    bind $startup_dismiss_w <Return> "
+    $startup_dismiss_w configure -s  tate active -relief sunken
+    update idletasks   
+    after 100	       
+    $startup_dismiss_w	invoke
+  " 
+    
+    pack $startup_label_w \
+	$startup_date_w \
   	$startup_info1_w \
-    $startup_info2_w \
-    $login_frame_w \
+	$startup_info2_w \
+	$login_frame_w \
   	$startup_dismiss_w \
   	-side top -padx 5 -pady 5
-
-  update idletask
-
-  tkwait visibility $startup_box_w
-  tkwait visibility $startup_dismiss_w
-
-  set oldFocus [focus]
-  set oldGrab [grab current $startup_box_w]
-  if {$oldGrab != ""} {
-    set grabStatus [grab status $oldGrab]
-  }
-  grab $startup_box_w
-  focus $startup_dismiss_w
-
-  update idletask
-#    after 2000 {
-#	set private(dismiss) 1
-#    }
-
-  update idletask
-  tkwait variable private(dismiss)
-
-  catch {focus $oldFocus}
-  catch {
-    # It's possible that the window has already been destroyed,
+    
+    update idletask
+    
+    tkwait visibility $startup_box_w
+    tkwait visibility $startup_dismiss_w
+    
+    set oldFocus [focus]
+    set oldGrab [grab current $startup_box_w]
+    if {$oldGrab != ""} {
+	set grabStatus [grab status $oldGrab]
+    }
+    grab $startup_box_w
+    focus $startup_dismiss_w
+    
+    update idletask
+    #    after 2000 {
+    #	set private(dismiss) 1
+    #    }
+    
+    update idletask
+    tkwait variable private(dismiss)
+    
+    catch {focus $oldFocus}
+    catch {
+	# It's possible that the window has already been destroyed,
   	# hence this "catch".  Delete the Destroy handler so that
   	# tkPriv(button) doesn't get reset by it.
-
-    bind $startup_box_w <Destroy> {}
-    destroy $startup_box_w
-  }
-
-  if {$oldGrab != ""} {
-    if {$grabStatus == "global"} {
-      grab -global $oldGrab
-    } else {
-      grab $oldGrab
+	
+	bind $startup_box_w <Destroy> {}
+	destroy $startup_box_w
     }
-  }
-
-  return
+    
+    if {$oldGrab != ""} {
+	if {$grabStatus == "global"} {
+	    grab -global $oldGrab
+	} else {
+	    grab $oldGrab
+	}
+    }
+    
+    return
 }
 #=============================================================================
 
 proc login_dialog {} {
-  global private login_box_w
-  global username
-
-  set login_box_w [toplevel .settings -class Dialog]
-  wm resizable $login_box_w false false
-  wm group $login_box_w .
-  wm sizefrom $login_box_w program
-  wm transient $login_box_w [winfo toplevel [winfo parent $login_box_w]]
-
-  set login_label_w [label $login_box_w.label \
-      -text {  Login  }]
-  set login_frame_w [frame $login_box_w.frame \
-      -relief sunken \
-      -borderwidth 2]
-  set login_user_l [label $login_frame_w.user_l -text "Username:"]
-  set login_user_e [ttk::entry $login_frame_w.user_e -textvariable tmpusername]
-  set login_passwd_l [label $login_frame_w.passwd_l -text "Password:"]
-  set login_passwd_e [ttk::entry $login_frame_w.passwd_e  -show {*} -textvariable tmppasswd]
-
-  grid $login_user_l -column 0 -row 0
-  grid $login_user_e -column 1 -row 0
-  grid $login_passwd_l -column 0 -row 1
-  grid $login_passwd_e -column 1 -row 1
-
-  pack $login_label_w -fill x -expand 1 -side top
-  pack $login_frame_w -fill both -expand 1
-
-  set button_frame_w [frame $login_box_w.button_frame]
-  set cancel_button_w [button $button_frame_w.cancel \
-    -text "Cancel" \
-    -command {
-      destroy $login_box_w
-    }]
+    global private login_box_w
+    global username
+    
+    set login_box_w [toplevel .settings -class Dialog]
+    wm resizable $login_box_w false false
+    wm group $login_box_w .
+    wm sizefrom $login_box_w program
+    wm transient $login_box_w [winfo toplevel [winfo parent $login_box_w]]
+    
+    set login_label_w [label $login_box_w.label \
+			   -text {  Login  }]
+    set login_frame_w [frame $login_box_w.frame \
+			   -relief sunken \
+			   -borderwidth 2]
+    set login_user_l [label $login_frame_w.user_l -text "Username:"]
+    set login_user_e [ttk::entry $login_frame_w.user_e -textvariable tmpusername]
+    set login_passwd_l [label $login_frame_w.passwd_l -text "Password:"]
+    set login_passwd_e [ttk::entry $login_frame_w.passwd_e  -show {*} -textvariable tmppasswd]
+    
+    grid $login_user_l -column 0 -row 0
+    grid $login_user_e -column 1 -row 0
+    grid $login_passwd_l -column 0 -row 1
+    grid $login_passwd_e -column 1 -row 1
+    
+    pack $login_label_w -fill x -expand 1 -side top
+    pack $login_frame_w -fill both -expand 1
+    
+    set button_frame_w [frame $login_box_w.button_frame]
+    set cancel_button_w [button $button_frame_w.cancel \
+			     -text "Cancel" \
+			     -command {
+				 destroy $login_box_w
+			     }]
     set ok_button_w [button $button_frame_w.ok \
-      -text "OK" \
-      -default active \
-      -command {
-        set username $tmpusername
-        set aespasswd [aes::aes -dir encrypt -key $aeskey $tmppasswd]
-#        write_settings $settings_fname $incl_geom $incl_surfs $incl_lights
-        destroy $login_box_w
-      }]
+			 -text "OK" \
+			 -default active \
+			 -command {
+			     set username $tmpusername
+			     set aespasswd [aes::aes -dir encrypt -key $aeskey $tmppasswd]
+			     #        write_settings $settings_fname $incl_geom $incl_surfs $incl_lights
+			     destroy $login_box_w
+			 }]
     bind $ok_button_w <Return> "
-      $ok_button_w configure -state active -relief sunken
-      update idletasks
-      after 100
-      $ok_button_w invoke
-  	"
-
+      $ok_button_w configur    e -state active -relief sunken
+      update idletasks	       
+      after 100		       
+      $ok_button_w invoke      
+  	"	   
+    
     grid columnconfigure $button_frame_w 0 -minsize 25
     grid columnconfigure $button_frame_w 1 -minsize 120
     grid columnconfigure $button_frame_w 2 -minsize 25
@@ -231,22 +235,22 @@ proc login_dialog {} {
     grid columnconfigure $button_frame_w 4 -minsize 25
     grid $cancel_button_w -row 0 -column 1 -sticky we -pady 5
     grid $ok_button_w -row 0 -column 3 -sticky we -pady 5
-
+    
     pack $button_frame_w -fill x -expand 1 -side bottom
-
+    
     raise $login_box_w
-
+    
     tkwait visibility $login_box_w
     tkwait visibility $ok_button_w
-
+    
     set oldFocus [focus]
     set oldGrab [grab current $login_box_w]
     if {$oldGrab != ""} {
-      set grabStatus [grab status $oldGrab]
+	set grabStatus [grab status $oldGrab]
     }
     grab $login_box_w
-#    focus $ok_button_w
-
+    #    focus $ok_button_w
+    
     return
 }
 
@@ -277,7 +281,7 @@ set header_w [ttk::frame .header -padding 5]
 set login_label [ttk::label $header_w.login_label -text "User: "]
 set login_name [ttk::label $header_w.login_name -textvariable username]
 set login_button [ttk::button $header_w.login_button -text "login..." -command {
-  login_dialog
+    login_dialog
 }]
 
 set tabs_w [ttk::notebook .tabs_w]
